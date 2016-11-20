d9ece33e9c09c9b00e8483d0278b5d3ca121fd05
DESKTOP-SBDVMCV\home <lus6@hanmail.net>
2016-11-20 15:22:18 +0900

    161120 - camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+    public Transform targetTr;
+
+    float distanceUp = 4f;
+    float distanceAway = 7f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = targetTr.position + Vector3.up * distanceUp -
+            Vector3.forward;
+	}
+}
---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using HedgehogTeam.EasyTouch;
+
+public class Gamemanager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Gesture current = EasyTouch.current;
+        if(current.type == EasyTouch.EvtType.On_SimpleTap) // 한번 터치시
+        {
+
+        }
+	}
+}
