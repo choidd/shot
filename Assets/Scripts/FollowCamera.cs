6b0e825a1dcb035aea065c124df0a1f0e39a0595
DESKTOP-SBDVMCV\home <lus6@hanmail.net>
2016-11-21 15:46:07 +0900

    161121 - camera complete

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -4,19 +4,18 @@ using System.Collections;
 public class FollowCamera : MonoBehaviour {
     public Transform targetTr;
 
-    float distanceUp = 4f;
+    float distanceUp = 2f;
     float distanceAway = 7f;
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
+
 	void LateUpdate () {
-        transform.position = Vector3.Lerp(transform.position,
+        /*
+            transform.position = Vector3.Lerp(transform.position,
             targetTr.position - (targetTr.forward * distanceAway) +
             (Vector3.up * distanceUp),
             Time.deltaTime * 30f);
-        transform.LookAt(targetTr.position);
+            */
+        //transform.LookAt(targetTr.position);
 	}
 }
--- Assets/Scripts/Player/MovePlayer.cs
+++ Assets/Scripts/Player/MovePlayer.cs
@@ -4,19 +4,29 @@ using HedgehogTeam.EasyTouch;
 public class MovePlayer : MonoBehaviour {
 
     NavMeshAgent nav;
-
+    Gesture current;
+    Camera MC; // main camera
+    Ray ray;
+    RaycastHit hit;
 	// Use this for initialization
 	void Start () {
         nav = GetComponent<NavMeshAgent>();
+        MC = GetComponentInChildren<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Gesture current = EasyTouch.current;
+        current = EasyTouch.current;
         if(current.type == EasyTouch.EvtType.On_SimpleTap)
         {
-            Vector3 tmp = Camera.main.WorldToScreenPoint(current.position);
-            nav.SetDestination(tmp);
+            ray = Camera.main.ScreenPointToRay(current.position);
+            Physics.Raycast(ray, out hit);
+            move();
         }
 	}
+
+    void move()
+    {
+        nav.SetDestination(hit.point);
+    }
 }
