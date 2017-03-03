90121e45ae7fb4c41ca54927a44405c8bf9f53b7
choidd <lus6@hanmail.net>
2017-03-03 18:19:47 +0900

    google login complete

---
+++ Assets/Scripts/FaceBookManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceBookManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
