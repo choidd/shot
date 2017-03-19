ae81a7cdbccffa7d29b9d3e4e8b3edc9b6e62b22
DESKTOP-SBDVMCV\home <lus6@hanmail.net>
2017-03-19 23:43:09 +0900

    170319

--- Assets/Scripts/PopupManager.cs
+++ Assets/Scripts/PopupManager.cs
@@ -7,38 +7,38 @@ public class PopupManager : MonoBehaviour {
 
 	public static PopupManager Instance { set; get; }
 
-    public CanvasGroup canvasGroup;
-    private Transform uiroot;
-    public void Start()
-    {
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-        canvasGroup.alpha = 0;
-        canvasGroup.blocksRaycasts = false;
-        canvasGroup.interactable = false;
+    private Transform PopupBackground;
 
+    static private PopupManager instance;
+
+    void Start()
+    {
+        instance = this;
     }
 
-    public void showPopup(string title, string message)
+    public void showPopup(string ObjName)
     {
-        if(uiroot == null)
+        PopupBackground = GameObject.Find(ObjName).transform;
+        StartCoroutine(StartShowPopup());
+    }
+
+    public IEnumerator StartShowPopup()
+    {
+        float timeStart = Time.time;
+        while (true)
         {
-            uiroot = GameObject.FindGameObjectWithTag("UIRoot").transform;
+            float timePassed = Time.time - timeStart;
+            float rate = timePassed / 0.5f;
 
-            transform.SetParent(uiroot);
+            PopupBackground.localPosition = new Vector3(0f, 1500f - 1500f * rate, 0f);
 
-            canvasGroup.alpha = 1;
-            canvasGroup.blocksRaycasts = true;
-            canvasGroup.interactable = true;
+            if (timePassed > 0.5f)
+            {
+                PopupBackground.localPosition = new Vector3(0f, 0f, 0f);
+                break;
+            }
+
+            yield return new WaitForFixedUpdate();
         }
-    }
-
-    public void OnClick()
-    {
-        canvasGroup.alpha = 0;
-        canvasGroup.blocksRaycasts = false;
-        canvasGroup.interactable = false;
-
-        transform.SetParent(uiroot.parent);
     }
 }
